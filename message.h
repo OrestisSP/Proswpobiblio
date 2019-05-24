204f5d71be461c6efe23c06826c08d6adf9b9c06
OrestisSP <50988686+OrestisSP@users.noreply.github.com>
2019-05-24 14:51:08 +0300

    Update message.h

--- message.h
+++ message.h
@@ -1,1 +1,20 @@
-asdasd
+#include <iostream>
+#include <string>
+#include<ctime>
+
+using namespace std;
+
+class Message
+{
+	struct tm 
+	{
+		int tm_sec;
+		int tm_min;
+		int tm_hour;
+		int tm_mday;
+		int tm_mon;
+		int tm_year;
+	}
+	private :
+		
+}
